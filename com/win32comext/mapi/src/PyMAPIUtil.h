b1843d72fe8c44a657ac8b3a3c6ff4b6666a53f6
Avasam <samuel.06@hotmail.com>
2026-05-01 11:02:38 -0400

    isapi and mapi header fixes

--- com/win32comext/mapi/src/PyMAPIUtil.h
+++ com/win32comext/mapi/src/PyMAPIUtil.h
@@ -5,7 +5,46 @@
 // Which actually has some differences in param types, causing build failures
 #include <MAPIDefS.h>
 #include <MAPICode.h>
+
+// HACK/TODO: This is a big copy from com/win32comext/mapi/src/mapiguids.cpp
+// Why can't we use that file or deduplicate instead?
+
+// PythonCOM.h includes <initguid.h> which sets INITGUID before PyMAPIUtil.h is
+// processed. MAPIGuid.h only declares an IID when (!INITGUID || USES_IID_<name>),
+// and its MAPIGUID_H guard fires under INITGUID, blocking later re-inclusion via
+// mapiguids.cpp. Define all USES_IID_* here so every IID is declared on first pass.
+#define USES_IID_IMAPISession
+#define USES_IID_IMAPIStatus
+#define USES_IID_IMAPITable
+#define USES_IID_IMAPIProp
+#define USES_IID_IMAPIFolder
+#define USES_IID_IMessage
+#define USES_IID_IMsgStore
+#define USES_IID_IMAPIContainer
+#define USES_IID_IMAPIProgress
+#define USES_IID_IAttachment
+#define USES_IID_IProfAdmin
+#define USES_IID_IMAPIAdviseSink
+#define USES_IID_IAddrBook
+#define USES_IID_IMAPIPropData
+#define USES_IID_IMailUser
+#define USES_IID_IDistList
+#define USES_IID_IABContainer
+#define USES_IID_IProfSect
+#define USES_IID_IMsgServiceAdmin
+#define USES_IID_IProviderAdmin
+#define USES_PS_PUBLIC_STRINGS
+#define USES_PS_MAPI
+#define USES_PS_ROUTING_EMAIL_ADDRESSES
+#define USES_PS_ROUTING_ADDRTYPE
+#define USES_PS_ROUTING_DISPLAY_NAME
+#define USES_PS_ROUTING_ENTRYID
+#define USES_PS_ROUTING_SEARCH_KEY
 #include <MAPIGuid.h>
+
+#define USES_IID_IMsgServiceAdmin2
+#define USES_IID_IMessageRaw
+#include <MAPIAux.h>
 #include <MAPITags.h>
 
 #include <MAPIX.h>
